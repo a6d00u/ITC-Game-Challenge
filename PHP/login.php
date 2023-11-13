3415bedbd54305c47c7d34d6e9f6c381dec162e0
ABDOU <abdouchaibdraa@outlook.com>
2023-11-13 03:05:38 +0100

    login

--- PHP/login.php
+++ PHP/login.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/CSS/loginstyle.css">
     <title>Login</title>
 </head>
 <body>
-    
+    <div class="container-login">
+        <h2>Login Page</h2><br/>
+        <div class="form-container">
+            <form action="" method="post">
+                <label for="email">E-mail</label>
+                <input type="email" name="email">
+                <label for="password">Password</label>
+                <input type="password" name="password">
+                <input type="submit" id="submit">
+            </form>
+            <span>Don't Have Account ? <a href="signup.php">Signup</a></span>
+        </div>
+    </div>
 </body>
 </html>
